f128d7e568a1d57e6e7b893b4eb793a99caa7959
Tony <jozeftony46@gmail.com>
2024-02-07 22:14:38 +0530

    created the first vertical box

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -1,10 +1,13 @@
 import { Box,Container } from '@mui/material'
-
+// import Assets from './Assets/Clouds - 16495.mp4'
 
 
  function ContainerA() {
   return (
-    
+    <div>
+        {/* <video>
+            <source src="./Assets/Clouds - 16495.mp4" type="video/mp4" />
+        </video> */}
     <Container 
     sx={{
     display:"flex",
@@ -13,24 +16,42 @@ import { Box,Container } from '@mui/material'
     height:"100vh"
     }}>
 
-<Box
-  sx={{
+    <Box
+    sx={{
     backgroundColor:"#030005",
     color:"white",
-    width:"1600px",
+    width:"2000px",
     height:"700px",
     textAlign:"center",
-    padding:"20px",
+    padding:"10px",
     borderRadius:"50px",
     display:"flex",
     flexDirection:"column",
     justifyContent:"space-around",
     boxShadow: "rgb(0, 0, 0) 0px 22px 70px 4px;",
-  }}
->
-    cdjvbl
-      </Box>
+     }}
+    >
+
+  <Box  
+  sx={{
+    backgroundColor:"green",
+    width:"400px",
+    height:"600px",
+    margin:"40px" ,
+    borderRadius:"50px",
+    
+    
+
+    }}>
+   main box vertical box
+  </Box>
+
+
+ 
+    </Box>
     </Container>
+    </div>
+
     
   
   );
