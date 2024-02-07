5d5e35219e7ce3c58d15b1d8d24febe36363ead8
Tony <jozeftony46@gmail.com>
2024-02-07 20:18:52 +0530

    made container file and added some code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
+import ContainerA from "./Components/Container";
 function App() {
   return (
-    <>
+    
       <div>
-        
+        <ContainerA></ContainerA>
       </div>
-    </>
   );
 }
 
---
+++ src/Components/Container.jsx
@@ -0,0 +1,40 @@
+import { Box,Container } from '@mui/material'
+
+
+
+ function ContainerA() {
+  return (
+    
+    <Container 
+    sx={{
+    display:"flex",
+    justifyContent: "center",
+    alignItems:"center",
+    height:"100vh"
+    }}>
+
+<Box
+  sx={{
+    backgroundColor:"#030005",
+    color:"white",
+    width:"300px",
+    height:"400px",
+    textAlign:"center",
+    padding:"20px",
+    borderRadius:"90px",
+    display:"flex",
+    flexDirection:"column",
+    justifyContent:"space-around",
+    boxShadow: "rgb(0, 0, 0) 0px 22px 70px 4px;",
+  }}
+>
+    cdjvbl
+      </Box>
+    </Container>
+    
+  
+  );
+}
+
+
+export default Container;
