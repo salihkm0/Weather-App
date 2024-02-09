399785bf1454f2935b2f2750fa7890d85ef2fe8e
Ameer Basha.K <bashabb738@gmail.com>
2024-02-09 13:13:23 +0530

    copy proccessing 2

--- src/ui/LeftsideBoxStyle.jsx
+++ src/ui/LeftsideBoxStyle.jsx
@@ -1,35 +1,35 @@
 export const LeftsideBoxStyle = {
-    // margin: "10px",
-    // padding:" 0 10px",
-    // textAlign:"center",
-    // backgroundColor: "",
-    
-    width: "400px",
-    height: "540px",
-    borderRadius: "20px",
-    display: "flex",
-    justifyContent: "center",
-    alignItems:"end",
-    textAlign:"center",
-   
-  }
-  export const SideInnerBoxStyle = {
-    // padding:"10px",
-    // justifyContent:"center",
-    backgroundColor: "#11121168",
-    width: "170px",
-    height: "100px",
-    margin: "10px",
-    borderRadius: "10px",
+  // margin: "10px",
+  // padding:" 0 10px",
+  // textAlign:"center",
+  backgroundColor: "#11121130",
+  border:"2px solid #0000006e",
+  width: "400px",
+  height: "540px",
+  borderRadius: "20px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems:"end",
+  textAlign:"center",
+ 
+}
+export const SideInnerBoxStyle = {
+  // padding:"10px",
+  // justifyContent:"center",
+  backgroundColor: "#0000002e",
+  width: "170px",
+  height: "100px",
+  margin: "10px",
+  borderRadius: "10px",
 
-  }
+}
 
-  export const InnerBox ={
-    backgroundColor:"",
-    width:"95%",
-    height:"280px",
-    borderRadius: "10px",
-    marginBottom: "10px",
-    padding:"0 10px"
-  }
+export const InnerBox ={
+  backgroundColor:"",
+  width:"95%",
+  height:"280px",
+  borderRadius: "10px",
+  marginBottom: "10px",
+  padding:"0 10px"
+}
 
