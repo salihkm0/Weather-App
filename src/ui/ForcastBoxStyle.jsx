71aa52417c2604013f5ddd68cbb14f7ec151e4f7
Ameer Basha.K <bashabb738@gmail.com>
2024-02-09 13:12:03 +0530

    copy proccessing 1

--- src/ui/ForcastBoxStyle.jsx
+++ src/ui/ForcastBoxStyle.jsx
@@ -1,76 +1,75 @@
 export const ForcastInnerBoxStyle ={
-    background:"#4c474782",
-    height:"95px",
-    minWidth:"80px",
-    borderRadius: "20px",
-    textAlign:"center",
-    margin:"7px",   
+  background:"#4c474782",
+  height:"95px",
+  minWidth:"80px",
+  borderRadius: "20px",
+  textAlign:"center",
+  margin:"7px",   
 }
 
 export const ForcastBoxStyle ={
 
-  background:"#0b0b0b35",
-  backdropFilter:"blur(10px)",
-  msOverflowStyle: "none",
-  height:"180px",
-  maxWidth:"570px",
-  borderRadius: "20px",
-  display:"flex",
-  alignItems:"end",
-  justifyContent:"center",
+background:"#00000031",
+msOverflowStyle: "none",
+height:"180px",
+maxWidth:"570px",
+borderRadius: "20px",
+display:"flex",
+alignItems:"end",
+justifyContent:"center",
 }
 
 export const ForcastSubBoxStyle ={
-  height:"123px",
-  maxWidth:"550px",
-  borderRadius: "20px",
-  marginBottom:"14px",
-  display:"flex",
-  alignItems:"end",
-  overflow:"auto",
-  overflowY:"hidden",
-  "&::-webkit-scrollbar": {
-      height:"4px",
-  },
-  "&::-webkit-scrollbar-track": {
-    background: "#f1f1f1",
-    backgroundFilter:"blur",
-    borderRadius:"2px",
-  },
-  "&::-webkit-scrollbar-thumb": {
-    background: "#756",
-    backgroundFilter:"blur",
-    borderRadius:"2px"
-  },
-  "&::-webkit-scrollbar-thumb:hover": {
-    background: "#678",
-  }, 
+height:"123px",
+maxWidth:"550px",
+borderRadius: "20px",
+marginBottom:"14px",
+display:"flex",
+alignItems:"end",
+overflow:"auto",
+overflowY:"hidden",
+"&::-webkit-scrollbar": {
+    height:"4px",
+},
+"&::-webkit-scrollbar-track": {
+  background: "#f1f1f1",
+  backgroundFilter:"blur",
+  borderRadius:"2px",
+},
+"&::-webkit-scrollbar-thumb": {
+  background: "#756",
+  backgroundFilter:"blur",
+  borderRadius:"2px"
+},
+"&::-webkit-scrollbar-thumb:hover": {
+  background: "#678",
+}, 
 
 }
 
 export const FBhourly={
-  textAlign:"center"
+textAlign:"center"
 }
 
 
 
 
 export const UVindexStyle ={
-    background:"#0b0b0b81",
-    height:"170px",
-    width:"100%",
-    borderRadius: "20px",
-    // overflow:"hidden"
+  background:"#00000031",
+  height:"170px",
+  width:"100%",
+  borderRadius: "20px",
+  // overflow:"hidden"
 
 }
 
 export const styledHr ={
-  width:"100%",
-  margin:"0px"
+width:"100%",
+margin:"0px"
 }
 
 export const FBhead ={
-  fontSize:"23px",
-  textAlign:"start",
-  justifyContent:"end"
+fontSize:"23px",
+textAlign:"start",
+justifyContent:"end"
 }
